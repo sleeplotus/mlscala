3f57f15257f4694a5f7821bfcb2674d60f9f495e
wangpengrobot <wangpengrobot@qq.com>
2017-11-21 10:49:22 +0800

    commit wp 06

--- src/main/scala/com/vrv/ml/classes/Point.scala
+++ src/main/scala/com/vrv/ml/classes/Point.scala
@@ -1,6 +1,6 @@
 package com.vrv.ml.classes
 
-class Point {
+class Point(val value: Double, val unit: String) {
 
   private var _x = 0
   private var _y = 0
@@ -19,5 +19,48 @@ class Point {
   }
 
   private def printWarning = println("WARNING: Out of bounds")
+
+  def apply(value: Double, unit: String): Point = {
+    println("class")
+    new Point(value, unit)
+  }
+
+}
+
+object Point {
+  def apply(value: Double, unit: String): Point = {
+    println("object")
+    new Point(value, unit)
+  }
+
+  def unapply(point: Point): Option[(Double, String)] = {
+    if (point == null) {
+      None
+    }
+    else {
+      Some(point.value, point.unit)
+    }
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val aa = new Test1()
+    val aa1 = Test1()
+
+    val point = Point(30.2, "USD")
+    Point.unapply(point).get
+    val Point(amount, unit) = point
+    println(amount)
+    println(unit)
+
+    point match {
+      case Point(amount, "USD") => println("$" + amount)
+      case _ => println("No match.")
+    }
+
+  }
+}
+
+case class Test1() {
 
 }
