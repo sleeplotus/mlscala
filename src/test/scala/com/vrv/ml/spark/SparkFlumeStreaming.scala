74c7ca38b6133598883dabd3da9605cab9dfb1c4
wangpengrobot <wangpengrobot@qq.com>
2018-02-02 15:11:01 +0800

    SparkFlumeStreaming

--- src/test/scala/com/vrv/ml/spark/SparkFlumeStreaming.scala
+++ src/test/scala/com/vrv/ml/spark/SparkFlumeStreaming.scala
@@ -10,11 +10,15 @@ class SparkFlumeStreaming {
 
   @Test
   def sparkFlumeStreaming(): Unit = {
-    val conf = new SparkConf().setAppName("SparkFlumeTest").setMaster("local[2]")
-    val ssc = new StreamingContext(conf, Seconds(10))
-//    val flumeStream = FlumeUtils.createPollingStream(ssc, "hostname", "")
-
-
+    val conf = new SparkConf().setAppName("SparkFlumeTest").setMaster("local[3]")
+    val ssc = new StreamingContext(conf, Seconds(5))
+    val flumeStream = FlumeUtils.createPollingStream(ssc, "192.168.2.118", 9090)
+    //    flumeStream.foreachRDD(rdd => rdd.foreach(println))
+    //    flumeStream.print()
+    flumeStream.count().map(cnt => "Received " + cnt + " flume events." ).print()
+    // Start
+    ssc.start()
+    ssc.awaitTermination()
   }
 
 }
