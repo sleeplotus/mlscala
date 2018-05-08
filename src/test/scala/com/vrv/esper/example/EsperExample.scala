4f00ab20bf4d6e0e8664131dd7c7940cdd9032f6
wangpengrobot <wangpengrobot@qq.com>
2018-05-08 18:28:35 +0800

    esper

--- src/test/scala/com/vrv/esper/example/EsperExample.scala
+++ src/test/scala/com/vrv/esper/example/EsperExample.scala
@@ -29,12 +29,58 @@ class EsperExample {
         override def update(newEvents: Array[EventBean], oldEvents: Array[EventBean]): Unit = {
           val name: String = newEvents(0).get("name").asInstanceOf[String]
           val age: Int = newEvents(0).get("age").asInstanceOf[Int]
-          println(s"String.format(Name: $name, Age: $age")
+          println(s"String.format(Name: $name, Age: $age)")
         }
       }
     )
     // Step Four: Send Events
     engine.getEPRuntime.sendEvent(PersonEvent("Peter", 10))
+  }
+
+  @Test
+  def createEventTypeByJavaObjectEplTest(): Unit = {
+    // Step One: Obtain Engine Instance
+    val engine: EPServiceProvider = EPServiceProviderManager.getDefaultProvider
+    // Step Two: Provide Information on Input Events
+    val schema: String = "create schema PersonEvent as (name string, age int)"
+    engine.getEPAdministrator.createEPL(schema)
+    // Step Three: Create EPL Statements and Attach Callbacks
+    val epl: String = "select name, age from PersonEvent"
+    val statement: EPStatement = engine.getEPAdministrator.createEPL(epl)
+    statement.addListener(
+      new UpdateListener() {
+        override def update(newEvents: Array[EventBean], oldEvents: Array[EventBean]): Unit = {
+          val name: String = newEvents(0).get("name").asInstanceOf[String]
+          val age: Int = newEvents(0).get("age").asInstanceOf[Int]
+          println(s"String.format(Name: $name, Age: $age)")
+        }
+      }
+    )
+    // Step Four: Send Events
+    engine.getEPRuntime.sendEvent(PersonEvent("Peter", 10))
+  }
+
+  @Test
+  def createEventTypeByObjectArrayEplTest(): Unit = {
+    // Step One: Obtain Engine Instance
+    val engine: EPServiceProvider = EPServiceProviderManager.getDefaultProvider
+    // Step Two: Provide Information on Input Events
+    val schema: String = "create objectarray schema PersonEvent as (name string, age int)"
+    engine.getEPAdministrator.createEPL(schema)
+    // Step Three: Create EPL Statements and Attach Callbacks
+    val epl: String = "select name, age from PersonEvent"
+    val statement: EPStatement = engine.getEPAdministrator.createEPL(epl)
+    statement.addListener(
+      new UpdateListener() {
+        override def update(newEvents: Array[EventBean], oldEvents: Array[EventBean]): Unit = {
+          val name: String = newEvents(0).get("name").asInstanceOf[String]
+          val age: Int = newEvents(0).get("age").asInstanceOf[Int]
+          println(s"String.format(Name: $name, Age: $age)")
+        }
+      }
+    )
+    // Step Four: Send Events
+    engine.getEPRuntime.sendEvent(Array[AnyRef]("Peter", Integer.valueOf(10)), "PersonEvent")
   }
 
 }
