b4c423ae52acaae11a9bf2c998e6d65413ae79a5
wangpengrobot <wangpengrobot@qq.com>
2018-05-09 14:56:05 +0800

    esper

--- src/test/scala/com/vrv/esper/example/EsperExample.scala
+++ src/test/scala/com/vrv/esper/example/EsperExample.scala
@@ -140,6 +140,35 @@ class EsperExample {
     engine.getEPRuntime.sendEvent(Array[AnyRef]("Peter", Integer.valueOf(10), Array[AnyRef]("南京市", "13888888888", Integer.valueOf(2018))), "PersonEvent")
   }
 
+  @Test
+  def objectArrayNestedPropertiesArrayTest(): Unit = {
+    // Step One: Obtain Engine Instance
+    val engine: EPServiceProvider = EPServiceProviderManager.getDefaultProvider
+    // Step Two: Provide Information on Input Events
+    val schemas: String ="""create objectarray schema extraInfoFields as (address string, phone string, uid int);
+                           |create objectarray schema PersonEvent as (name string, age int, extraInfo extraInfoFields)""".stripMargin
+    schemas.split(";").foreach(schema => {
+      engine.getEPAdministrator.createEPL(schema)
+    })
+    // Step Three: Create EPL Statements and Attach Callbacks
+    val epl: String = "select name, age, extraInfo.address, extraInfo.phone as phone, extraInfo.uid from PersonEvent"
+    val statement: EPStatement = engine.getEPAdministrator.createEPL(epl)
+    statement.addListener(
+      new UpdateListener() {
+        override def update(newEvents: Array[EventBean], oldEvents: Array[EventBean]): Unit = {
+          val name: String = newEvents(0).get("name").asInstanceOf[String]
+          val age: Int = newEvents(0).get("age").asInstanceOf[Int]
+          val address: String = newEvents(0).get("extraInfo.address").asInstanceOf[String]
+          val phone: String = newEvents(0).get("phone").asInstanceOf[String]
+          val uid: Int = newEvents(0).get("extraInfo.uid").asInstanceOf[Int]
+          println(s"String.format(Name: $name, Age: $age, Address: $address, Phone: $phone, Uid: $uid)")
+        }
+      }
+    )
+    // Step Four: Send Events
+    engine.getEPRuntime.sendEvent(Array[AnyRef]("Peter", Integer.valueOf(10), Array[AnyRef]("南京市", "13888888888", Integer.valueOf(2018))), "PersonEvent")
+  }
+
 }
 
 
