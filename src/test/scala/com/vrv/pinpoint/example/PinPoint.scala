a6d91d3e7748fe8e33b376f80a0b2cbaafde1916
SleepLotus <wangpengrobot@126.com>
2018-09-19 15:17:34 +0800

    PinPoint AgentInfo Example

--- src/test/scala/com/vrv/pinpoint/example/PinPoint.scala
+++ src/test/scala/com/vrv/pinpoint/example/PinPoint.scala
@@ -1,5 +1,8 @@
 package com.vrv.pinpoint.example
 
+import com.vrv.pinpoint.example.common.hbase.RowMapper
+import com.vrv.pinpoint.example.web.mapper.AgentInfoMapper
+import com.vrv.pinpoint.example.web.vo.AgentInfo
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.client._
 import org.apache.hadoop.hbase.util.Bytes
@@ -8,41 +11,68 @@ import org.junit._
 
 class PinPoint {
 
-  @Test
-  def agentInfo(): Unit = {
+  /**
+    * Finds table
+    * @param tableName Table Name
+    * @param rowMapper Row Mapper
+    * @tparam T Generic Type
+    */
+  def findTable[T](tableName: String, rowMapper: RowMapper[T]): Unit = {
     // Configuration
     val conf: Configuration = HBaseConfiguration.create()
-    val ZOOKEEPER_QUORUM = "192.168.2.16"
-    conf.set("hbase.zookeeper.quorum", ZOOKEEPER_QUORUM)
+    val zookeeperQuorum = "192.168.2.16"
+    conf.set("hbase.zookeeper.quorum", zookeeperQuorum)
     val connection = ConnectionFactory.createConnection(conf)
 
     // TableName
-    val table = connection.getTable(TableName.valueOf(Bytes.toBytes("default:AgentInfo")))
+    val table = connection.getTable(TableName.valueOf(Bytes.toBytes("default:" + tableName)))
 
     // Scan example
     println("Scan Example:")
-    val scan = table.getScanner(new Scan())
-    val results = scan.iterator()
+    val resultScanner: ResultScanner = table.getScanner(new Scan())
+    val results = resultScanner.iterator()
+    var rowNum:Int = 0
     while (results.hasNext) {
-      printRow(results.next())
+      rowNum += rowNum
+      println(rowMapper.mapRow(results.next(), rowNum).toString)
     }
+
+    // Releases resources
+    resultScanner.close()
     table.close()
     connection.close()
   }
 
   /**
-    * Prints row
+    * AgentInfo
+    */
+  @Test
+  def agentInfo(): Unit = {
+    findTable[AgentInfo]("AgentInfo", new AgentInfoMapper())
+  }
+
+  /**
+    * ApplicationIndex
+    */
+  @Test
+  def applicationIndex(): Unit = {
+
+  }
+
+
+  /**
+    * Map ApplicationIndex row
     *
     * @param result HBase查询结果
     */
-  def printRow(result: Result): Unit = {
+  def applicationIndexMapRow(result: Result): Unit = {
     val cells = result.rawCells()
-    println(Bytes.toString(result.getRow))
+    println(s"RowKey：${Bytes.toString(result.getRow)}")
     for (cell <- cells) {
       val col_name = Bytes.toString(CellUtil.cloneQualifier(cell))
-      val col_value = Bytes.toString(CellUtil.cloneValue(cell))
-      println(s"$col_name")
-      println(s"$col_value")
+      val serviceTypeCode: Short = Bytes.toShort(CellUtil.cloneValue(cell));
+      println(s"ColumnName：$col_name")
+      println(s"ColumnValue：$serviceTypeCode")
     }
     println("=============================================================================")
   }
---
+++ src/test/scala/com/vrv/pinpoint/example/web/mapper/AgentInfoMapper.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2016 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.vrv.pinpoint.example.web.mapper;
+
+
+import com.vrv.pinpoint.example.common.PinpointConstants;
+import com.vrv.pinpoint.example.common.buffer.Buffer;
+import com.vrv.pinpoint.example.common.buffer.FixedBuffer;
+import com.vrv.pinpoint.example.common.hbase.HBaseTables;
+import com.vrv.pinpoint.example.common.hbase.RowMapper;
+import com.vrv.pinpoint.example.common.server.bo.AgentInfoBo;
+import com.vrv.pinpoint.example.common.server.bo.JvmInfoBo;
+import com.vrv.pinpoint.example.common.server.bo.ServerMetaDataBo;
+import com.vrv.pinpoint.example.common.util.BytesUtils;
+import com.vrv.pinpoint.example.common.util.TimeUtils;
+import com.vrv.pinpoint.example.web.vo.AgentInfo;
+import org.apache.hadoop.hbase.client.Result;
+
+/**
+ * @author HyunGil Jeong
+ */
+public class AgentInfoMapper implements RowMapper<AgentInfo> {
+
+    @Override
+    public AgentInfo mapRow(Result result, int rowNum) throws Exception {
+
+        byte[] rowKey = result.getRow();
+        String agentId = BytesUtils.safeTrim(BytesUtils.toString(rowKey, 0, PinpointConstants.AGENT_NAME_MAX_LEN));
+        long reverseStartTime = BytesUtils.bytesToLong(rowKey, HBaseTables.AGENT_NAME_MAX_LEN);
+        long startTime = TimeUtils.recoveryTimeMillis(reverseStartTime);
+
+        byte[] serializedAgentInfo = result.getValue(HBaseTables.AGENTINFO_CF_INFO, HBaseTables.AGENTINFO_CF_INFO_IDENTIFIER);
+        byte[] serializedServerMetaData = result.getValue(HBaseTables.AGENTINFO_CF_INFO, HBaseTables.AGENTINFO_CF_INFO_SERVER_META_DATA);
+        byte[] serializedJvmInfo = result.getValue(HBaseTables.AGENTINFO_CF_INFO, HBaseTables.AGENTINFO_CF_INFO_JVM);
+
+        final AgentInfoBo.Builder agentInfoBoBuilder = createBuilderFromValue(serializedAgentInfo);
+        agentInfoBoBuilder.setAgentId(agentId);
+        agentInfoBoBuilder.setStartTime(startTime);
+
+        if (serializedServerMetaData != null) {
+            agentInfoBoBuilder.setServerMetaData(new ServerMetaDataBo.Builder(serializedServerMetaData).build());
+        }
+        if (serializedJvmInfo != null) {
+            agentInfoBoBuilder.setJvmInfo(new JvmInfoBo(serializedJvmInfo));
+        }
+        return new AgentInfo(agentInfoBoBuilder.build());
+    }
+
+    private AgentInfoBo.Builder createBuilderFromValue(byte[] serializedAgentInfo) {
+        final Buffer buffer = new FixedBuffer(serializedAgentInfo);
+        final AgentInfoBo.Builder builder = new AgentInfoBo.Builder();
+        builder.setHostName(buffer.readPrefixedString());
+        builder.setIp(buffer.readPrefixedString());
+        builder.setPorts(buffer.readPrefixedString());
+        builder.setApplicationName(buffer.readPrefixedString());
+        builder.setServiceTypeCode(buffer.readShort());
+        builder.setPid(buffer.readInt());
+        builder.setAgentVersion(buffer.readPrefixedString());
+        builder.setStartTime(buffer.readLong());
+        builder.setEndTimeStamp(buffer.readLong());
+        builder.setEndStatus(buffer.readInt());
+        // FIXME - 2015.09 v1.5.0 added vmVersion (check for compatibility)
+        if (buffer.hasRemaining()) {
+            builder.setVmVersion(buffer.readPrefixedString());
+        }
+        return builder;
+    }
+}
