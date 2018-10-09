ad199550a3ce895678f7cd7ff2e147342806d3df
SleepLotus <wangpengrobot@126.com>
2018-10-09 15:26:06 +0800

    PinPoint HBase Schema TraceV2

--- src/test/scala/com/vrv/pinpoint/example/schema/TraceV2.scala
+++ src/test/scala/com/vrv/pinpoint/example/schema/TraceV2.scala
@@ -1,5 +1,17 @@
 package com.vrv.pinpoint.example.schema
 
+import com.vrv.pinpoint.example.schema.util.SchemaUtils
+import com.vrv.pinpoint.example.web.mapper.SpanMapperV2
+import org.junit.Test
+
 class TraceV2 {
+
+  /**
+    * TraceV2
+    */
+  @Test
+  def traceV2(): Unit = {
+    SchemaUtils.findTable("TraceV2", new SpanMapperV2())
+  }
 
 }
