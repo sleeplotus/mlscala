97ec5e719d2f601da0c8e2de873ac8e6177edd23
SleepLotus <wangpengrobot@126.com>
2018-10-09 17:40:03 +0800

    PinPoint HBase Schema AgentEvent

---
+++ src/test/scala/com/vrv/pinpoint/example/schema/AgentEvent.scala
@@ -0,0 +1,15 @@
+package com.vrv.pinpoint.example.schema
+
+import com.vrv.pinpoint.example.schema.util.SchemaUtils
+import com.vrv.pinpoint.example.web.mapper.AgentEventMapper
+import org.junit.Test
+
+class AgentEvent {
+  /**
+    * AgentInfo
+    */
+  @Test
+  def agentEvent(): Unit = {
+    SchemaUtils.findTable("AgentEvent", new AgentEventMapper())
+  }
+}
