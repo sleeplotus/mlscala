8abb009fd8f5fbdb08a2d910f96d5680f09eb9c9
SleepLotus <wangpengrobot@126.com>
2018-10-10 11:42:44 +0800

    PinPoint HBase Schema ApplicationTraceIndex Scatter

--- src/test/scala/com/vrv/pinpoint/example/schema/ApplicationTraceIndex.scala
+++ src/test/scala/com/vrv/pinpoint/example/schema/ApplicationTraceIndex.scala
@@ -1,7 +1,7 @@
 package com.vrv.pinpoint.example.schema
 
 import com.vrv.pinpoint.example.schema.util.SchemaUtils
-import com.vrv.pinpoint.example.web.mapper.TransactionIdMapper
+import com.vrv.pinpoint.example.web.mapper.{TraceIndexScatterMapper2, TraceIndexScatterMapper3, TransactionIdMapper}
 import org.junit.Test
 
 class ApplicationTraceIndex {
@@ -9,7 +9,23 @@ class ApplicationTraceIndex {
     * ApplicationTraceIndex
     */
   @Test
-  def mapRow(): Unit = {
+  def scanTraceIndex(): Unit = {
     SchemaUtils.findTable("ApplicationTraceIndex", new TransactionIdMapper())
+  }
+
+  /**
+    * ApplicationTraceIndex
+    */
+  @Test
+  def scanTraceScatter(): Unit = {
+    SchemaUtils.findTable("ApplicationTraceIndex", new TraceIndexScatterMapper2(1, 99999))
+  }
+
+  /**
+    * ApplicationTraceIndex
+    */
+  @Test
+  def scanTraceScatterData(): Unit = {
+    SchemaUtils.findTable("ApplicationTraceIndex", new TraceIndexScatterMapper3(1, 99999, 100, 100))
   }
 }
