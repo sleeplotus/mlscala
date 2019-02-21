4fe11b3f39f47afdaad7db7166ea6001c895b20e
SleepLotus <wangpengrobot@126.com>
2019-02-21 14:13:59 +0800

    Spark Streaming

--- src/test/scala/com/vrv/ml/spark/streaming/SparkSteaming.scala
+++ src/test/scala/com/vrv/ml/spark/streaming/SparkSteaming.scala
@@ -9,17 +9,40 @@ import org.junit._
   * @date 2/20/2019
   * @company VRV
   */
+//noinspection ScalaDocUnknownTag,ScalaDocParserErrorInspection
 class SparkSteaming {
 
   @Test
-  def tcpSparkStreamingTest: Unit = {
+  def socketSparkStreamingTest(): Unit = {
     // Create a local StreamingContext with two working thread and batch interval of 1 second.
     // The master requires 2 cores to prevent a starvation scenario.
     val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
     val ssc = new StreamingContext(conf, Seconds(1))
 
     // Create a DStream that will connect to hostname:port, like localhost:9999
-    val lines = ssc.socketTextStream("localhost", 9999)
+    val lines = ssc.socketTextStream("192.168.2.15", 9999)
+
+    // Split each line into words
+    val words = lines.flatMap(_.split(" "))
+
+    // Count each word in each batch
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_ + _)
+
+    // Print the first ten elements of each RDD generated in this DStream to the console
+    wordCounts.print()
+
+    ssc.start()             // Start the computation
+    ssc.awaitTermination()  // Wait for the computation to terminate
+  }
+
+  @Test
+  def fileSparkStreamingTest(): Unit = {
+    // Create a local StreamingContext with two working thread and batch interval of 1 second.
+    // The master requires 2 cores to prevent a starvation scenario.
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(conf, Seconds(1))
+    val lines = ssc.textFileStream("d:/dataDirectory")
 
     // Split each line into words
     val words = lines.flatMap(_.split(" "))
